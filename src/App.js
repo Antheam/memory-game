abcbde005b1e78f91cdf2cc0940a7ea56f51bd3e
Anthea Merton <47039803+Antheam@users.noreply.github.com>
2025-11-07 16:08:01 +0000

    Add custom react hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Background from './components/Background';
+import useGetImages from './hooks/useGetImages';
+// const BASE_URL = '/v1/search?query=nature&orientation=square';
 
 function App() {
+    useGetImages();
     return (
         <>
             <Background />
--- src/hooks/useGetImages.js
+++ src/hooks/useGetImages.js
@@ -1,5 +1,34 @@
+import { useEffect } from 'react';
+
 const BASE_URL = 'https://api.pexels.com/v1/search';
 
-const useGetImages = () => {};
+const getRandomPage = () => Math.round(Math.random() * (10 - 1) + 1);
+
+const useGetImages = () => {
+    const buildUrl = () => {
+        let url = new URL('https://api.pexels.com/v1/search');
+        url.search = new URLSearchParams({
+            query: 'nature', //change to a variable
+            orientation: 'square',
+            size: 'small',
+            per_page: 2, //change this to a variable
+            page: getRandomPage(),
+        });
+        // console.log('url', url);
+        return url;
+    };
+
+    const fetchPics = () => {
+        fetch(buildUrl(), {
+            headers: {
+                Authorization: 'SV5y6V6aMIn7YWRxeniV2BlzxX1wzpaG4DirVslvmi5iOymsMMvsYiMP',
+            },
+        });
+    };
+
+    useEffect(() => {
+        fetchPics();
+    }, []);
+};
 
 export default useGetImages;
